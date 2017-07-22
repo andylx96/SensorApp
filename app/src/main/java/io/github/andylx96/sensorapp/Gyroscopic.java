7eed941b45ad6bc78a8dd28b4c847d7e4965e2a0
andylx96 <andylx96@gmail.com>
2017-07-22 05:02:14 -0400

    basic firebase connection, gyro push

--- app/src/main/java/io/github/andylx96/sensorapp/Gyroscopic.java
+++ app/src/main/java/io/github/andylx96/sensorapp/Gyroscopic.java
@@ -9,18 +9,23 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Gyroscopic extends AppCompatActivity  implements SensorEventListener {
 
 
         TextView accelText;
         SensorManager sm;
         Sensor accelSensor;
+        private DatabaseReference mDatabase;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_gyroscopic);
 
+            mDatabase = FirebaseDatabase.getInstance().getReference();
 
             sm = (SensorManager)getSystemService(SENSOR_SERVICE);
             accelSensor = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
@@ -32,6 +37,8 @@ public class Gyroscopic extends AppCompatActivity  implements SensorEventListene
                 Toast.makeText(this, "Sensor Not Found", Toast.LENGTH_LONG).show();
             }
 
+
+
         }
 
 
@@ -42,6 +49,12 @@ public class Gyroscopic extends AppCompatActivity  implements SensorEventListene
             accelText.setText("Gyroscope Sensor: \n X: " + String.valueOf(event.values[0]) +
             "\n Y: " + String.valueOf(event.values[1]) +
                     "\n Z: "+String.valueOf(event.values[2]) );
+
+            mDatabase.child("X").push().setValue(event.values[0]);
+            mDatabase.child("Y").push().setValue(event.values[1]);
+            mDatabase.child("Z").push().setValue(event.values[2]);
+
+
         }
 
         @Override
